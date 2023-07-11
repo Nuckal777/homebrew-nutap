454c3f182f808737c7d786baac53b1308f53aea7
Nuckal777 <nuckal777@gmail.com>
2023-07-11 11:04:02 +0200

    Update nuq

--- Formula/nuq.rb
+++ Formula/nuq.rb
@@ -1,10 +1,10 @@
 class Nuq < Formula
   desc "Multi-format frontend to jq"
   homepage "https://github.com/Nuckal777/nuq"
-  url "https://github.com/Nuckal777/nuq/archive/refs/tags/v0.1.1.tar.gz"
+  url "https://github.com/Nuckal777/nuq/archive/refs/tags/v0.1.2.tar.gz"
   head "https://github.com/Nuckal777/nuq.git"
-  version "0.1.1"
-  sha256 "f8e6d68c1d2cb441c7f89d8b7e454c1437306fc129a439e82f1f0ab12898d02d"
+  version "0.1.2"
+  sha256 "a34c7e8751ce9c108ee5a63d308287d379de4fe81329b614b16db25d4997c316"
   license "Unlicense"
 
   depends_on "rust" => :build
