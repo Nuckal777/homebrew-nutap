3818a9ff1669b6b1a016887aa0d44a8c699ea1e4
Nuckal777 <nuckal777@gmail.com>
2024-04-16 18:37:30 +0200

    Update promdump

--- Formula/promdump.rb
+++ Formula/promdump.rb
@@ -1,11 +1,11 @@
 class Promdump < Formula
   desc "Dumps prometheus queries"
   homepage "https://github.com/sapcc/promdump"
-  url "https://github.com/sapcc/promdump/archive/refs/tags/v0.2.0.tar.gz"
+  url "https://github.com/sapcc/promdump/archive/refs/tags/v0.3.0.tar.gz"
   head "https://github.com/sapcc/promdump.git"
   # also update version in install
-  version "0.2.0"
-  sha256 "042d245cacd40b33da9fa01cdd97f80dce13545b744c8ad5d78052271b2d6b1d"
+  version "0.3.0"
+  sha256 "2196a11444404cc2d6101362c1f373a222eef447fda5c1b6532057bcb143ca07"
   license "Apache-2.0"
 
   depends_on "go" => :build
@@ -13,7 +13,7 @@ class Promdump < Formula
   depends_on "make" => :build
 
   def install
-    system("make", "VERSION=v0.2.0")
+    system("make", "VERSION=v0.3.0")
     bin.install "promdump"
   end
 
